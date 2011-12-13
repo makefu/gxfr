3561b4a0959855342734d605ec4e0fc2ac9ccc94
tjt1980@gmail.com <tjt1980@gmail.com@b1ef51b2-318d-a6a2-9305-983e6c4ada9f>
2011-12-13 05:28:33 +0000

    updated multiple
    git-svn-id: http://ptscripts.googlecode.com/svn/trunk@17 b1ef51b2-318d-a6a2-9305-983e6c4ada9f

--- linkedin.py
+++ linkedin.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python -tt
 
-
-# to do
-# try/catch in search function and around while statement so that errors dont break the script
-
-
 # linkedin.py queries Google for LinkedIn profiles associated to the given company
 # and confirms current employment by scraping and analyzing the profile page. It
 # then reports back the users full name, possible email prefix, and job title.
@@ -15,14 +10,18 @@
 import re, urllib, urllib2 
 
 def search(q, start = 0):
-  url = 'http://www.google.com/m/search?'
-  query = urllib.urlencode({'q':q, 'start':start})
-  global verbose
-  if verbose: print "[Query]", url + query
-  request = urllib2.Request(url + query)
-  requestor = urllib2.build_opener()
-  request.addheaders = [('User-agent', 'Mozilla/5.0')]
-  content = requestor.open(request).read()
+  try:
+    url = 'http://www.google.com/m/search?'
+    query = urllib.urlencode({'q':q, 'start':start})
+    global verbose
+    if verbose: print '[Query] %s%s' % (url, query)
+    request = urllib2.Request(url + query)
+    requestor = urllib2.build_opener()
+    request.addheaders = [('User-agent', 'Mozilla/5.0')]
+    content = requestor.open(request).read()
+  except:
+    print '[Error] %s%s' % (url, query)
+    content = ' '
   return content
 
 def printhelp():
@@ -62,7 +61,11 @@ for i in range(2,len(sys.argv),1):
     tofile = True
     filename = sys.argv[i+1]
 
-if tofile: file = open(filename, 'w')
+if tofile:
+  if os.path.exists(filename):
+    file = open(filename, 'a')
+  else:
+    file = open(filename, 'w')
 query = 'site:linkedin.com/in "' + company+ '" ' + addsearch
 names_urls = []
 records = []
@@ -70,44 +73,51 @@ index = 0
 print ''
 print '[*] Resolving employees using the following query:', query
 print ''
-print 'First Name'.ljust(15) + 'Last Name'.ljust(18) + 'Possible Email'.ljust(20) + 'Position'
-print '=========='.ljust(15) + '========='.ljust(18) + '=============='.ljust(20) + '========'
+print 'First Name'.ljust(15) + 'Last Name'.ljust(18) + 'Title'
+print '=========='.ljust(15) + '========='.ljust(18) + '====='
 while True:
-  if googledepth != '*':
-    if index >= int(googledepth):
+  try:
+    if googledepth != '*':
+      if index >= int(googledepth):
+        break
+    result = search(query, index*10)
+    names_urls = re.findall(";u=http://\www\.linkedin\.com/in/[a-zA-Z0-9/-]+.>[ |a-zA-Z0-9,.-]+", result)
+    try:
+      for token in names_urls:
+        token = token.lower()
+        names_array = token.split(">")
+        url_array = names_array[0].split("=")
+        url = url_array[1][:-1]
+        name = names_array[1].split(" ")
+        fname = name[0]
+        lname = name[1]
+        content = urllib.urlopen(url).read().split('\n')
+        i = 0
+        for item in content:
+          # if current job exists
+          if item.find('headline-title title') != -1:
+            job = content[i+3]
+            # if current job is named company
+            if job.lower().find(company.lower()) != -1:
+              record = fname.ljust(15) + lname.ljust(18) + job.lstrip(' ')
+              if not record in records:
+                records.append(record)
+                print record
+                if tofile:
+                  file.write(fname + '|' + lname + '|' + fname[:1] + lname + '|' + job.lstrip(' ') + '\n')
+                  file.flush()
+              else:
+                if verbose: print '[Duplicate found] %s %s' % (fname, lname)
+              break
+          i += 1
+    except:
+      print '[Notice] Skipping %s. Confirm manually.' % (token)
+    index += 1
+    if not 'Next page' in result:
+      print ''
+      print '[*] End of Google search results reached.'
       break
-  result = search(query, index*10)
-  names_urls = re.findall(';u=http://\www\.linkedin\.com/in/[a-zA-Z0-9/-]+.>[ |a-zA-Z0-9,.-]+', result)
-  for token in names_urls:
-    token = token.lower()
-    names_array = token.split(">")
-    url_array = names_array[0].split("=")
-    url = url_array[1][:-1]
-    name = names_array[1].split(" ")
-    fname = name[0]
-    lname = name[1]
-    content = urllib.urlopen(url).read().split('\n')
-    i = 0
-    for item in content:
-      # if current job exists
-      if item.find('headline-title title') != -1:
-        job = content[i+3]
-        # if current job is named company
-        if job.lower().find(company.lower()) != -1:
-          record = fname.ljust(15) + lname.ljust(18) + fname[:1] + lname.ljust(19) + job.lstrip(' ')
-          if not record in records:
-            records.append(record)
-            print record
-            if tofile:
-              file.write(fname + '|' + lname + '|' + fname[:1] + lname + '|' + job.lstrip(' ') + '\n')
-              file.flush()
-          else:
-            if verbose: print '[Duplicate found]', fname, lname
-          break
-      i += 1
-  index += 1
-  if not 'Next page' in result:
-    print ''
-    print '[*] End of Google search results reached.'
-    break
+  except KeyboardInterrupt:
+    print 'Exiting...'
+    if tofile: file.close()
 if tofile: file.close()
