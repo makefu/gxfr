9f5d78ca34533d5472d0105dafa0164f5a5da39c
tjt1980@gmail.com <tjt1980@gmail.com@b1ef51b2-318d-a6a2-9305-983e6c4ada9f>
2012-04-17 04:39:08 +0000

    updated linkedin.py
    git-svn-id: http://ptscripts.googlecode.com/svn/trunk@32 b1ef51b2-318d-a6a2-9305-983e6c4ada9f

--- linkedin.py
+++ linkedin.py
@@ -7,10 +7,11 @@
 # Code is based heavily on the core functionality of userpass.py by Mark Baggett.
 # http://pauldotcom.com/userpass.py
 
-import re, urllib, urllib2, time
+import re, urllib, urllib2, time, traceback, htmllib, sqlite3
 
 def search(q, start = 0):
   try:
+    content = ''
     url = 'http://www.google.com/m/search?'
     query = urllib.urlencode({'q':q, 'start':start})
     global verbose
@@ -19,10 +20,19 @@ def search(q, start = 0):
     requestor = urllib2.build_opener()
     request.addheaders = [('User-agent', 'Mozilla/5.0')]
     content = requestor.open(request).read()
+  except KeyboardInterrupt:
+    pass
   except:
-    print '[Error] %s%s' % (url, query)
-    content = ' '
+    print '-'*60
+    traceback.print_exc(file=sys.stdout)
+    print '-'*60
   return content
+
+def unescape(s):
+  p = htmllib.HTMLParser(None)
+  p.save_bgn()
+  p.feed(s)
+  return p.save_end()
 
 def printhelp():
   print """
@@ -32,10 +42,10 @@ Options:
   -g [#]        - The number of google pages to parse looking for employees of the Company (default is indef)
   -s            - Additional search words
   -v            - Be verbose
-  -o [filename] - Output or append to easily parsable file
+  -o [filename] - Output or append to sqlite3 database
 Example:
-  ./linkedin.py 'Company Name' -g 5 -o report
-                 - Querying to a google depth of 5, searching for employees of company, and outputing results to a file.
+  ./linkedin.py 'Company Name' -g 5 -o report.db
+                 - Querying to a google depth of 5, searching for employees of company, and outputting results to a db.
   """
 
 import os, sys
@@ -49,6 +59,7 @@ googledepth = '*'
 addsearch = ''
 verbose = False
 tofile = False
+delay = 10
 
 for i in range(2,len(sys.argv),1):
   if sys.argv[i] == '-g':
@@ -62,65 +73,68 @@ for i in range(2,len(sys.argv),1):
     filename = sys.argv[i+1]
 
 if tofile:
-  if os.path.exists(filename):
-    file = open(filename, 'a')
-  else:
-    file = open(filename, 'w')
-#query = 'site:linkedin.com "%s" %s' % (company, addsearch)
-query = 'site:linkedin.com/in "%s" %s' % (company, addsearch)
-names_urls = []
-records = []
+  # initialize the database
+  con = sqlite3.connect(filename)
+  cur = con.cursor()
+  cur.execute('create table if not exists targets (fname text, lname text, title text)')
+  con.commit()
+
+query = 'site:linkedin.com "%s" %s' % (company, addsearch)
+#query = 'site:linkedin.com/in "%s" %s' % (company, addsearch)
 index = 0
 print ''
 print '[*] Resolving employees using the following query:', query
 print ''
 print 'First Name'.ljust(15) + 'Last Name'.ljust(18) + 'Title'
 print '=========='.ljust(15) + '========='.ljust(18) + '====='
-while True:
-  try:
+try:
+  while True:
     if googledepth != '*':
       if index >= int(googledepth):
         break
     result = search(query, index*10)
-    #names_urls = re.findall(";u=http://\www\.linkedin\.com/[a-zA-Z0-9/-]+.>[ |a-zA-Z0-9,.-]+", result)
-    names_urls = re.findall(";u=http://\www\.linkedin\.com/in/[a-zA-Z0-9/-]+.>[ |a-zA-Z0-9,.-]+", result)
-    try:
-      for token in names_urls:
-        token = token.lower()
-        names_array = token.split(">")
-        url_array = names_array[0].split("=")
-        url = url_array[1][:-1]
-        name = names_array[1].split(" ")
-        fname = name[0]
-        lname = name[1]
-        content = urllib.urlopen(url).read().split('\n')
-        i = 0
-        for item in content:
-          # if current job exists
-          if item.find('headline-title title') != -1:
-            job = content[i+3]
+    names_urls = re.findall(";u=http://\www\.linkedin\.com/[a-zA-Z0-9/-]+.>[ |a-zA-Z0-9,.-]+", result)
+    #names_urls = re.findall(";u=http://\www\.linkedin\.com/in/[a-zA-Z0-9/-]+.>[ |a-zA-Z0-9,.-]+", result)
+    for token in names_urls:
+      token = token.lower()
+      url = token.split(">")[0].split("=")[1][:-1]
+      content = urllib.urlopen(url).read().split('\n')
+      i = 0
+      fname = ''
+      lname = ''
+      for item in content:
+        try:
+          # get the first and last names
+          if item.find('class="full-name"') != -1:
+            #import pdb; pdb.set_trace()
+            m = re.search('given-name">(.+?)<.+family-name">(.+?)<', item)
+            fname = unescape(m.group(1))
+            lname = unescape(m.group(2))
+          # get current job title
+          elif item.find('headline-title title') != -1:
+            job = unescape(content[i+3])
             # if current job is named company
             if job.lower().find(company.lower()) != -1:
-              record = fname.ljust(15) + lname.ljust(18) + job.lstrip(' ')
-              if not record in records:
-                records.append(record)
-                print record
-                if tofile:
-                  file.write(fname + '|' + lname + '|' + job.lstrip(' ') + '\n')
-                  file.flush()
-              else:
-                if verbose: print '[Duplicate found] %s %s' % (fname, lname)
-              break
+              if tofile:
+                cur.execute('insert into targets values (?,?,?)', (fname,lname,job))
+                con.commit()
+              print fname.ljust(15) + lname.ljust(18) + job.lstrip(' ')
+            break
           i += 1
-    except:
-      print '[Notice] Skipping %s. Confirm manually.' % (token)
-    index += 1
+        except:
+          print '-'*60
+          traceback.print_exc(file=sys.stdout)
+          print '-'*60
+          print '[Notice] Skipping %s. Confirm manually.' % (token)
+          continue
     if not 'Next page' in result:
-      print ''
-      print '[*] End of search results reached.'
+      print '\n[*] End of search results reached.'
       break
-    time.sleep(15)
-  except KeyboardInterrupt:
-    print 'Exiting...'
-    if tofile: file.close()
-if tofile: file.close()
+    index += 1
+    time.sleep(delay)
+except KeyboardInterrupt:
+  pass
+print '\nExiting...'
+if tofile:
+  cur.close()
+  con.close()
